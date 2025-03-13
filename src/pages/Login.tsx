72bc212365c6208711cefc0c3c1b7e4c92e4b4b9
arif013 <mdarif11099@gmail.com>
2025-03-13 06:30:35 +0530

    url added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,6 +21,7 @@ const Login = () => {
         e.preventDefault();
         try {
             const response = await axios.post('https://gs-987610-md-arif-backend.vercel.app/api/login', { email, password });
+            // const response = await axios.post('http://localhost:5000/api/login', { email, password });
             localStorage.setItem('token', response.data.token);
             navigate('/');
         } catch (err) {
